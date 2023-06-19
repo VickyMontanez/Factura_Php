0b04d6ad25664c52084918628d74e8c127cd036a
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-06-18 23:01:15 -0500

    feat: 🗃️ DB Connection
    The connection to the database was completed, dynamic and extended to the client class for a test

--- scripts/db/credentials.php
+++ scripts/db/credentials.php
@@ -1,8 +1,17 @@
 <?php
 /* Se declara una nueva clase */
-class credentials{
-    /* Se utiliza el trait "getInstance" */
-    use getInstance;
-    function __construct(){}
-}
+    abstract class credentials{
+        /* Se utiliza el trait "getInstance" */
+        /* use getInstance; */
+        protected $host = '172.16.48.210';
+        private $user = 'sputnik';
+        private $password = 'Sp3tn1kC@';
+        protected $dbname='db_hunter_facture';
+
+        public function __get($name){
+            return $this->{$name};
+        }
+    }
+
+    
 ?>
