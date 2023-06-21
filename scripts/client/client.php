c60c6411bde2510a2d872e97637ee3ad4a0f1fce
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-06-20 21:45:13 -0500

    feat: :sparkles: Add a Functional Crud for every Class
    Add a complety functional crud for every class using

--- scripts/client/client.php
+++ scripts/client/client.php
@@ -1,7 +1,9 @@
 <?php
 class client extends connect{
-    private $queryPost = 'INSERT INTO tb_client(identificacion,full_name,email,address,phone) VALUES(:cc,:name,:email,:direction,:cellphone)';
+    private $queryPost = 'INSERT INTO tb_client(client_id,client_fullname,client_email,client_address,client_phone) VALUES(:cc,:name,:email,:direction,:cellphone)';
     private $queryGetAll = 'SELECT * FROM tb_client';
+    private $queryDelete = 'DELETE FROM tb_client WHERE client_id = :clientId';
+    private $queryUpdate = 'UPDATE tb_client SET client_fullname = :fullName, client_email = :email, client_address = :address, client_phone = :phone WHERE identificacion = :clientId';
     private $message;
     use getInstance;
     function __construct(private $Identification, public $Full_Name, public $Email, private $Address, private $Phone){parent::__construct();}
@@ -24,6 +26,34 @@ class client extends connect{
         }catch(\PDOException $e) {
             $this->message = ["Code"=> $e->getCode(), "Message"=>$res->errorInfo()[2]];
         }finally{
+            print_r($this->message);
+        }
+    }
+    public function deleteClient($clientId){
+        try{
+            $res = $this->conx->prepare($queryDelete);
+            $res->bindValue(':clientId', $clientId);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => "Deleted data"];
+        } catch(\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function updateClient($clientId, $newFullName, $newEmail, $newAddress, $newPhone){
+        try{
+            $res = $this->conx->prepare($queryUpdate);
+            $res->bindValue(':fullName', $newFullName);
+            $res->bindValue(':email', $newEmail);
+            $res->bindValue(':address', $newAddress);
+            $res->bindValue(':phone', $newPhone);
+            $res->bindValue(':clientId', $clientId);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => "Updated data"];
+        } catch(\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
             print_r($this->message);
         }
     }
