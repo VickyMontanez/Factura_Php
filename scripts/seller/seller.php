d923ace6bfd3a14cb11a7b4145a50c48b814a2d9
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-06-20 21:46:43 -0500

    feat: :sparkles: Add a Functional Crud for every Class
    Add a complety functional crud for every class using database mysql

--- scripts/seller/seller.php
+++ scripts/seller/seller.php
@@ -1,9 +1,58 @@
 <?php
 /* Se declara una nueva clase */
-class seller{
-    /* Se utiliza el trait "getInstance" */
+class seller extends connect{
+    private $queryPost = 'INSERT INTO tb_seller(seller_id,seller_name) VALUES(:cc,:name)';
+    private $queryGetAll = 'SELECT * FROM tb_seller';
+    private $queryDelete = 'DELETE FROM tb_seller WHERE seller_id = :sellerId';
+    private $queryUpdate = 'UPDATE tb_seller SET seller_name = :sName WHERE seller_id = :sellerId';
+    private $message;
     use getInstance;
-    /* Y recibe un parametro público */
-    function __construct(public $Seller){}
+    function __construct(private $Seller_Id,public $Seller_Name){parent::__construct();}
+    public function postSeller(){
+        try{
+            $res = $this->conx->prepare($this->queryPost);  
+            $res->execute();
+            $this->message = ["Code"=>200+$res->rowCount(), "Message" => "Inserted data"];
+        }catch(\PDOException $e) {
+            $this->message = ["Code"=> $e->getCode(), "Message"=>$res->errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+    public function getAllSeller(){
+        try{
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $this->message = ["Code"=> 200, "Message"=> $res->fetchAll(PDO::FETCH_ASSOC)];
+        }catch(\PDOException $e) {
+            $this->message = ["Code"=> $e->getCode(), "Message"=>$res->errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+    public function deleteSeller($IdSeller){
+        try{
+            $res = $this->conx->prepare($queryDelete);
+            $res->bindValue(':sellerId', $IdSeller);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => "Deleted data"];
+        } catch(\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function updateSeller($IdSeller, $newName){
+        try{
+            $res = $this->conx->prepare($queryUpdate);
+            $res->bindValue(':sName', $newName);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => "Updated data"];
+        } catch(\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
 }
 ?>
