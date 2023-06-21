998369263101c6a668046dd71b8e4808c44ad9bb
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-06-20 21:43:53 -0500

    feat: :sparkles: Add a Functional Crud for every Class
    Add a complety functional crud for every class using

--- scripts/db/credentials.php
+++ scripts/db/credentials.php
@@ -6,8 +6,8 @@
         protected $host = '172.16.48.210';
         private $user = 'sputnik';
         private $password = 'Sp3tn1kC@';
-        protected $dbname='db_hunter_facture';
-
+        protected $dbname='db_hunter_facture_vicky';
+        
         public function __get($name){
             return $this->{$name};
         }
